25bcce2360d59e380f8d4e41f6ca7c737f043462
Dersu <148701933+dersuprogg@users.noreply.github.com>
2024-01-18 12:41:20 +0500

    Add smooth scrolling animation in JS

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ hamburgerBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   header.classList.remove("nav-open");
 });
+
+const contactLink = document.querySelector(".nav-list li:nth-child(3)");
+contactLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  const sectionEl = e.target.getAttribute("href");
+  document.querySelector(sectionEl).scrollIntoView({ behavior: "smooth" });
+});
